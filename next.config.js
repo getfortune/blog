f42be586fd049764f613a376f114bec1671d04ac
vincent <vincent@kreator-inc.com>
2024-12-13 15:56:16 +0800

    fix: next config

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/my-blog',
-  assetPrefix: '/my-blog/',
+  // 只在生产环境使用 basePath 和 assetPrefix
+  ...(process.env.NODE_ENV === 'production' ? {
+    basePath: '/blog',
+    assetPrefix: '/blog/',
+  } : {})
 }
 
 module.exports = nextConfig 
