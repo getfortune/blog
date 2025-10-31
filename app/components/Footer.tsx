e94e46f8bc4d10f63eb727351632df88881e1269
vincent <vincent@kreator-inc.com>
2025-10-31 14:40:53 +0800

    修改

--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 py-6">
-      <div className="container mx-auto px-4 text-center text-gray-600">
-        <p>© {new Date().getFullYear()} My Blog. All rights reserved.</p>
-      </div>
-    </footer>
-  )
-} 
